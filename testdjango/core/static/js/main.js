a68f406fe644cf18cfde102dfc4a3d8db491300b
raul <ra.medinaf@duocuc.c>
2023-01-19 22:29:24 -0300

    proyecto django 19-01

---
+++ testdjango/core/static/js/main.js
@@ -0,0 +1,52 @@
+console.log("Hola mundo como estan");
+var variable=true
+const constante=5;
+console.log(constante);
+variable=false;
+console.log(variable);
+
+// //operadores arismeticos
+var Num1=15;
+var Num2=3;
+var vNom="juan carlos";
+var vApellidos ="Osses Vidal";
+console.log("suma:",Num1+Num2);
+console.log("resta:",Num1-Num2);
+console.log("division:",Num1/Num2);
+console.log("multiplicacion:",Num1*Num2);
+console.log("nombre completo:"+ vNom+" " + vApellidos);
+console.log("resto de division:",Num1%Num2);
+
+// //sentencias condicionales
+if(Num1%2==0){
+    console.log("EL", Nun1,"ES numero par");
+}
+else { 
+    console.log("El", Num1,"Es numero impar");
+}
+
+// //operadores comparacion 
+// var vGlosa="el numero:"
+// if(Num1>Num2){
+//     console.log(vGlosa,Num1,"Es mayor que",Num2);
+// }
+// else if(Num1<Num2){
+//     console.log(vGlosa,Num2,"es menor que",Nun1);
+// }
+// else{console.log(vGlosa,Num1,"es igual a", Num2);
+// }
+
+// //sumar dos numeros
+// function suma(){
+//     var vfNum1 = document.getElementById("vNum1").value;
+//     var vfNum2 = document.getElementById("vNum2").value;
+
+
+//     if(vfNum1===""||vfNum2==="")
+//     {alert("Debe completar todos los datos");}
+//     else {var vfsuma=parseInt(vfNum1)+parseInt(vfNum2);
+//         document.getElementById("vResultado").value = vfsuma;
+//     }
+// }
+
+
